43abf71273423ad5724bfb83148d8de3550c9061
JackTheRipper2312 <arian.mirzazade@yahoo.com>
2022-03-06 16:08:22 +0100

    added evalBExpr function

--- Assignment.fsx
+++ Assignment.fsx
@@ -16,7 +16,7 @@ open AssignmentLexer
 
 // We define the evaluation function recursively, by induction on the structure
 // of arithmetic expressions (AST of type expr)
-let rec eval e =
+let rec evalAExpr e =
   match e with
     | Num(x) -> x
     | TimesExpr(x,y) -> eval(x) * eval (y)
@@ -26,29 +26,69 @@ let rec eval e =
     | PowExpr(x,y) -> eval(x) ** eval (y)
     | UPlusExpr(x) -> eval(x)
     | UMinusExpr(x) -> - eval(x)
+    | Var(x) -> 
+    | ListAExpr(x,y) -> 
+and evalCommand e =
+    match e with
+    | AssignVarExpr(x,v) -> 
+    | AssignArray(a,i,v) -> 
+    | Skip -> 
+    | CommandSeq(c1,c2) -> evalCommand c1
+                           evalCommand c2
+    | IfExpr((b,c) :: res) -> if evalBExpr(b) then evalCommand(c) else Skip
+    | DoExpr(g) -> 
+and evalGuardedCommand e =
+    match e with
+    | BoolGC(b,c) -> [(b,c)]
+    | GCSequence(g1,g2) -> (evalGuardedCommand g1) @ (evalGuardedCommand g2)
+
+
+let rec evalBExpr b =
+    match b with
+    | True -> true
+    | False -> false
+    | AndExpr(b1,b2) -> evalBExpr(b1) & evalBExpr(b2)
+    | OrExpr(b1,b2) -> let b1 = evalBExpr(b1)
+                       let b2 = evalBExpr(b2)
+                       b1 || b2
+    | SCAndExpr(b1,b2) -> evalBExpr(b1) && evalBExpr(b2)
+    | SCOrExpr(b1,b2) -> evalBExpr(b1) || evalBExpr(b2)
+    | NotExpr(b) -> not evalBExpr(b)
+    | EqExpr(a1,a2) -> evalAExpr(a1) == evalAExpr(a2)
+    | NotEqExpr(a1,a2) -> evalAExpr(a1) <> evalAExpr(a2)
+    | GrExpr(a1,a2) -> evalAExpr(a1) > evalAExpr(a2)
+    | GrEqExpr(a1,a2) -> evalAExpr(a1) >= a2
+    | LeExpr(a1,a2) -> evalAExpr(a1) < evalAExpr(a2)
+    | LeEqExpr(a1,a2) -> evalAExpr(a1) <= evalAExpr(a2)
+
+
+// if
+// true -> x = x+1
+// false -> x:=2
+// fi
 
 // We
-let parse input =
-    // translate string into a buffer of characters
-    let lexbuf = LexBuffer<char>.FromString input
-    // translate the buffer into a stream of tokens and parse them
-    let res = AssignmentParser.start AssignmentLexer.tokenize lexbuf
-    // return the result of parsing (i.e. value of type "expr")
-    res
+// let parse input =
+//     // translate string into a buffer of characters
+//     let lexbuf = LexBuffer<char>.FromString input
+//     // translate the buffer into a stream of tokens and parse them
+//     let res = AssignmentParser.start AssignmentLexer.tokenize lexbuf
+//     // return the result of parsing (i.e. value of type "expr")
+//     res
 
 // We implement here the function that interacts with the user
-let rec compute n =
-    if n = 0 then
-        printfn "Bye bye"
-    else
-        printf "Enter an arithmetic expression: "
-        try
-        // We parse the input string
-        let e = parse (Console.ReadLine())
-        // and print the result of evaluating it
-        printfn "Result: %f" (eval(e))
-        compute n
-        with err -> compute (n-1)
+// let rec compute n =
+//     if n = 0 then
+//         printfn "Bye bye"
+//     else
+//         printf "Enter an arithmetic expression: "
+//         try
+//         // We parse the input string
+//         let e = parse (Console.ReadLine())
+//         // and print the result of evaluating it
+//         printfn "Result: %f" (eval(e))
+//         compute n
+//         with err -> compute (n-1)
 
 // Start interacting with the user
-compute 3
+// compute 3
